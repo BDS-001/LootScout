70a7d7489928577b79edd31ac3e2f12a7244f3e2
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-07-13 18:12:41 -0400

    update: format steam game name

--- src/parsers/SteamParser.ts
+++ src/parsers/SteamParser.ts
@@ -1,10 +1,16 @@
 import { SteamAppUrlData } from '../shared/types';
 
+function formatAppName(appName: string) {
+	return appName.trim().replace('_', ' ');
+}
+
 export default function parseSteamPageUrl(): SteamAppUrlData {
 	const pattern = /store\.steampowered\.com\/app\/(?<appId>\d+)\/(?<appName>[\w-]+)/;
 	const match = window.location.href.match(pattern);
 	const appId = match?.groups?.appId || null;
-	const appName = match?.groups?.appName || null;
+	const appName = match?.groups?.appName ? formatAppName(match?.groups?.appName) : null;
+
+	if (appName) appName;
 
 	return { appId, appName };
 }
