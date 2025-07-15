1179abec12024d742cd58eeb0bb5dc00faa1ca57
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-07-15 13:43:18 -0400

    fix: url now replaces all instances of spaces

--- src/helpers/hltb.ts
+++ src/helpers/hltb.ts
@@ -1,4 +1,4 @@
 export function getHltbUrl(title: string): string {
-	const formattedTitle = title.trim().replace(' ', '%2520');
+	const formattedTitle = title.trim().replace(/ /g, '%2520');
 	return `https://howlongtobeat.com/?q=${formattedTitle}`;
 }
