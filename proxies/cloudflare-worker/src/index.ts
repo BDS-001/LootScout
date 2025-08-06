ba4b102f6c40d04a6f2a378b07c2143de421c487
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-08-05 22:59:29 -0400

    update: prettier formatting

--- proxies/cloudflare-worker/src/index.ts
+++ proxies/cloudflare-worker/src/index.ts
@@ -11,8 +11,41 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+function getCorsHeaders(origin: string): Record<string, string> {
+	return {
+		'Access-Control-Allow-Origin': origin,
+		'Access-Control-Allow-Methods': 'POST, OPTIONS',
+		'Access-Control-Allow-Headers': 'Content-Type',
+	};
+}
+
+function isExtensionOrigin(origin: string | null): boolean {
+	return !!(
+		origin?.startsWith('chrome-extension://') ||
+		origin?.startsWith('moz-extension://') ||
+		origin?.startsWith('safari-web-extension://')
+	);
+}
+
+function validateRequest(request: Request): Boolean {
+	if (!isExtensionOrigin(request.headers.get('origin'))) throw new Error('Forbidden');
+	if (request.method === 'OPTIONS') return false;
+	if (request.method !== 'POST') throw new Error('Method not allowed');
+	return true;
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request, _env, _ctx): Promise<Response> {
+		const origin = request.headers.get('origin');
+		try {
+			const validRequest = validateRequest(request);
+			if (!validRequest) {
+				return new Response(null, {
+					status: 204,
+					headers: getCorsHeaders(origin!),
+				});
+			}
+		} catch (error) {}
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
